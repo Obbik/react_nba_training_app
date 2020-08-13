d79d0e352d2beef71e735533c8b6aca6504fc005
Obbik <norbertdering@gmail.com>
2020-08-13 21:13:20 +0200

    bob

--- src/components/Articles/News/Post/index.js
+++ src/components/Articles/News/Post/index.js
@@ -3,3 +3,11 @@ import axios from "axios";
 import { URL } from "../../../../config";
 
 import styles from "../../articles.css";
+
+class NewsArticle extends Component {
+  render() {
+    return <div>article view</div>;
+  }
+}
+
+export default NewsArticle;
